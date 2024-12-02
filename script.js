63c53c91e66733171f431124d5b46ddc477ac977
NoorulSafrana <168085661+NoorulSafrana@users.noreply.github.com>
2024-12-02 11:02:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const homeElement=document.getElementById("home")
+function changeone()
+{
+    homeElement.style.backgroundColor="blue"
+}
+const aboutElement=document.getElementById("about")
+function changetwo()
+{
+    aboutElement.style.backgroundColor="blue"
+}
+const servicesElement=document.getElementById("services")
+function changethree()
+{
+    servicesElement.style.backgroundColor="blue"
+}
